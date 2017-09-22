790c49875782f9be12d3a8a6c37782fbc8570138
Guillermo Rauch <rauchg@gmail.com>
2017-09-22 02:27:37 +0200

    update wordpress

--- wordpress/src/run.sh
+++ wordpress/src/run.sh
@@ -3,6 +3,14 @@ _term()
 {
   kill -TERM "$child" 2> /dev/null
 }
+
+cat <<EOF >> /var/www/wordpress/wp-content/themes/twentyseventeen/functions.php
+add_filter('option_blogdescription', 'custom_option_description', 10, 1);
+function custom_option_description($value) {
+  return 'Just another WordPress site. Host id: `cat /proc/sys/kernel/random/uuid`';
+}
+EOF
+
 php-fpm &
 trap _term SIGTERM
 nginx
