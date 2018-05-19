06d4bc83ff77fac0a1c1e48931235e228f55ba51
Nathan Rajlich <nathan@tootallnate.net>
2018-05-18 20:03:51 -0700

    Add more dump

--- dump/server.js
+++ dump/server.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const env = {};
 for (const key of Object.keys(process.env).sort()) {
   if (key === 'AUTH_TOKEN') continue;
@@ -9,7 +10,20 @@ module.exports = async function(req, res) {
     url: req.url,
     method: req.method,
     headers: req.headers,
-    versions: process.versions,
-    env
+    process: {
+      pid: process.pid,
+      ppid: process.ppid,
+      versions: process.versions,
+      env
+    },
+    os: {
+      hostname: os.hostname(),
+      arch: os.arch(),
+      platform: os.platform(),
+      homedir: os.homedir(),
+      hostname: os.hostname(),
+      cpus: os.cpus(),
+      userInfo: os.userInfo()
+    }
   }
 };
